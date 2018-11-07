026aa112bdebea7dd81d0715fc1fbf8e854cfa28
dslab0915 <dslab0915@gmail.com>
2018-11-07 17:06:06 +0900

    ex_13 실습코드 추가

---
+++ ex_13/hidden.py
@@ -0,0 +1,12 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+# 트위터 개발자 등록 후, API 관련 Key를 생성해야 API를 이용할 수 있음
+# API 사용 목적에 대한 설명을 기입해야 개발자 등록이 가능!
+def oauth():
+    return {"consumer_key": "mvY0pD...veSwsu", # 생략
+            "consumer_secret": "IkskBnteL...xHc83ANpm4beAeod", # 생략
+            "token_key": "1710...eMDGrOzOROQ", # 생략
+            "token_secret": "8ym91...iWw5G7FGJyWPangSs" # 생략
+    }
