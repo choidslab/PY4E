8deca21cfa0642a35193347f3b2222147c80edb3
dslab0915 <dslab0915@gmail.com>
2018-11-26 22:53:21 +0900

    PY4E 데이터시각화 실습자료

---
+++ ex_16/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','ufp.pt','indiana.edu','berkeley.edu','uct.ac.za','mac.com','columbia.edu','gmail.com','unl.edu'],
+['2005-12',57,11,10,12,12,14,12,13,10,12],
+['2006-01',14,10,11,7,5,2,3,2,2,0]
+];
